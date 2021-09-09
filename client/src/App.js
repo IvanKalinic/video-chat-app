dceceb797bdf7753d8753829913768a4038c23c0
Ivan Kalinić <ivankalinic1998@gmail.com>
2021-09-09 16:03:44 +0200

    Finishing context

--- client/src/App.js
+++ client/src/App.js
@@ -2,17 +2,19 @@ import React from "react";
 import VideoPlayer from "./components/VideoPlayer";
 import Options from "./components/Options";
 import Notifications from "./components/Notifications";
-
+import { SocketProvider } from "./context/SocketContext";
 import "./App.scss";
 
 const App = () => {
   return (
     <div className="parent-container">
-      <div className="app-bar">Video Chat App</div>
-      <VideoPlayer />
-      <Options>
-        <Notifications />
-      </Options>
+      <SocketProvider>
+        <div className="app-bar">Video Chat App</div>
+        <VideoPlayer />
+        <Options>
+          <Notifications />
+        </Options>
+      </SocketProvider>
     </div>
   );
 };
